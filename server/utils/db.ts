32b3f040e0514f361b8426bef8662dc7c834b893
FashionCStar <fashioncstar@gmail.com>
2024-06-05 01:08:47 -0700

    update dashboard

---
+++ server/utils/db.ts
@@ -0,0 +1,20 @@
+import { drizzle as drizzleD1, DrizzleD1Database } from "drizzle-orm/d1"
+import { drizzle, BetterSQLite3Database } from "drizzle-orm/better-sqlite3"
+import Database from "better-sqlite3"
+
+let _db: DrizzleD1Database | BetterSQLite3Database | null = null
+
+export const useDb = () => {
+  if (!_db) {
+    if (process.env.DB) {
+      _db = drizzleD1(process.env.DB)
+    } else if (process.env.NODE_ENV !== "production") {
+      const sqlite = new Database("server/db/sqlite.db")
+      _db = drizzle(sqlite)
+    } else {
+      throw new Error("No database configured for production")
+    }
+  }
+
+  return _db
+}
